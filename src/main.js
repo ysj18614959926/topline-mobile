5a9e74123f8e7dd6e0ec4d3cff1de9983b20e972
ysj18614959926 <18614959926@163.com>
2019-07-08 15:50:09 +0800

    调整目录结构，安装vant

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'vant/lib/index.css' // 引入vant样式
+import { Button } from 'vant'
 Vue.config.productionTip = false
-
+Vue.use(Button)
 new Vue({
   router,
   store,
