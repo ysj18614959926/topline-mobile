0846a0cee8b7bbeb79515891b2187a1dbb525716
ysj18614959926 <18614959926@163.com>
2019-07-09 20:04:44 +0800

    适配rem

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,13 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {}
+const { sep } = require('path') // path.sep获取文件路径分隔符，win7和mac系统不一样'\\'或'/'
+module.exports = ({ file }) => {
+  let rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1 ? 37.5 : 75 // vant基于375像素开发，而一般ui设计稿是750像素
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        proList: ['*']
+      }
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
 import 'vant/lib/index.css' // 引入vant样式
-import { Button } from 'vant'
+import 'amfe-flexible' // 引入amfe-flexible设置 rem 基准值
 Vue.config.productionTip = false
-Vue.use(Button)
+Vue.use(Vant)
 new Vue({
   router,
   store,
