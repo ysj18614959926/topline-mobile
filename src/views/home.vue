02f62a5b2ec50e2cfa8310f3c2bb66f30aa2ef7d
ysj18614959926 <18614959926@163.com>
2019-07-09 20:52:11 +0800

    首页头部和底部

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>home</div>
+  <div>
+    <van-nav-bar
+        title="首页" fixed
+    />
+    <van-tabbar v-model="active" fixed>
+        <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+        <van-tabbar-item icon="chat-o">问答</van-tabbar-item>
+        <van-tabbar-item icon="video-o">视频</van-tabbar-item>
+        <van-tabbar-item icon="user-circle-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 <script>
+export default {
+  data () {
+    return {
+      active: 0
+    }
+  }
+}
 </script>
 <style>
 </style>
