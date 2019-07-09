f1f0975d3205066013d25cb3dec2e80d72ab0721
ysj18614959926 <18614959926@163.com>
2019-07-09 20:20:49 +0800

    login页面

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,7 +1,57 @@
 <template>
-  <div>login</div>
+  <div>
+    <van-nav-bar
+      title="登录"
+      class="loginNavBar"
+    />
+    <form class="loginBox">
+      <van-cell-group>
+          <van-field
+            v-model="user.mobile"
+            required
+            clearable
+            label="手机号"
+            placeholder="请输入手机号"
+          />
+          <van-field
+            v-model="user.code"
+            label="验证码"
+            placeholder="请输入验证码"
+            required
+          />
+        </van-cell-group>
+        <van-button type="info" @click.prevent='handelLogin'>登录</van-button>
+    </form>
+  </div>
 </template>
 <script>
+export default {
+  data () {
+    return {
+      user: {
+        mobile: '18614959926',
+        code: '123456'
+      }
+    }
+  },
+  methods: {
+    handelLogin () {
+      console.log('handelLogin click')
+    }
+  }
+}
 </script>
-<style>
+<style lang='less' scoped>
+.loginNavBar {
+  background-color: #3296fa;
+}
+.loginBox{
+  width: 100%;
+  box-sizing: border-box;
+  padding: 20px;
+  button {
+    margin-top: 20px;
+    width: 100%;
+  }
+}
 </style>
