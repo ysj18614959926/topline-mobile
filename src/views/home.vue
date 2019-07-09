29653cf3bb3c8e0bcaa8f1d47c0a5aebfb286ccc
ysj18614959926 <18614959926@163.com>
2019-07-09 21:08:24 +0800

    首页列表list

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,6 +3,25 @@
     <van-nav-bar
         title="首页" fixed
     />
+    <van-tabs v-model="active" class="channelBar">
+        <van-tab title="标签 1">
+            <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+            @load="onLoad"
+            >
+            <van-cell
+                v-for="item in list"
+                :key="item"
+                :title="item"
+            />
+            </van-list>
+        </van-tab>
+        <van-tab title="标签 2">内容 2</van-tab>
+        <van-tab title="标签 3">内容 3</van-tab>
+        <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
     <van-tabbar v-model="active" fixed>
         <van-tabbar-item icon="home-o">首页</van-tabbar-item>
         <van-tabbar-item icon="chat-o">问答</van-tabbar-item>
@@ -15,10 +34,30 @@
 export default {
   data () {
     return {
-      active: 0
+      active: 0,
+      loading: false,
+      finished: false,
+      list: []
+    }
+  },
+  methods: {
+    onLoad () {
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        this.loading = false
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 1000)
     }
   }
 }
 </script>
-<style>
+<style lang='less' scoped>
+.channelBar {
+  margin-top: 46px;
+  margin-bottom: 50px;
+}
 </style>
