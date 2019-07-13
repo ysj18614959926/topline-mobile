56037830c2f738ca790e475d487d816d0e7a7c49
ysj18614959926 <18614959926@163.com>
2019-07-13 14:34:06 +0800

    弹出框中获取我的频道列表

--- src/views/home/components/allChannels.vue
+++ src/views/home/components/allChannels.vue
@@ -4,7 +4,25 @@
     @input= '$emit("input", $event)'
     position="bottom"
     :style="{ height: '90%' }"
-    />
+    >
+    <div>我的频道<span>点击进入频道</span></div>
+    <van-grid>
+        <van-grid-item
+            v-for="item in userChannel"
+            :key="item.id"
+        >
+            <span>{{item.name}}</span>
+        </van-grid-item>
+    </van-grid>
+    <div>频道推荐<span>点击添加频道</span></div>
+    <van-grid>
+        <van-grid-item
+            v-for="value in 4"
+            :key="value"
+            text="文字"
+        />
+    </van-grid>
+    </van-popup>
 </template>
 <script>
 export default {
@@ -12,6 +30,10 @@ export default {
     value: {
       type: Boolean,
       default: false // 默认值
+    },
+    userChannel: {
+      type: Array,
+      default: () => [] // 必须死一个函数
     }
   },
   data () {
@@ -26,5 +48,9 @@ export default {
   }
 }
 </script>
-<style>
+<style scoped>
+div {
+    font-size: 12px;
+    margin-bottom: 30px;
+}
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -28,7 +28,7 @@
         <van-tabbar-item icon="video-o">视频</van-tabbar-item>
         <van-tabbar-item icon="user-circle-o">我的</van-tabbar-item>
     </van-tabbar>
-    <all-channels v-model='isShowDialog'></all-channels>
+    <all-channels v-model='isShowDialog' :user-channel='channels'></all-channels>
   </div>
 </template>
 <script>
